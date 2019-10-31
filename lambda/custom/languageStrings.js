5efcbd4396963b526ddf22af436c6962da640f1a
ArunSarva <49897489+ArunSarva@users.noreply.github.com>
2019-10-31 09:57:02 +0530

    Initial commit

---
+++ lambda/custom/languageStrings.js
@@ -0,0 +1,22 @@
+/* *
+ * We create a language strings object containing all of our strings.
+ * The keys for each string will then be referenced in our code, e.g. handlerInput.t('WELCOME_MSG').
+ * The localisation interceptor in index.js will automatically choose the strings
+ * that match the request's locale.
+ * */
+
+module.exports = {
+    en: {
+        translation: {
+            WELCOME_MSG: 'Welcome to Qwinix.',
+            HELLO_MSG: 'Hello Arun Namasthe how can I Help You',
+            BIRTHDAY_MSG: 'wish me on feb 28',
+            BIRTH_MSG: "Thanks, I'll remember that your birthday is ${month} ${day} ${year}",
+            HELP_MSG: 'You can say hello to me! How can I help?',
+            GOODBYE_MSG: 'Goodbye!',
+            REFLECTOR_MSG: 'You just triggered {{intentName}}',
+            FALLBACK_MSG: 'Sorry, I don\'t know about that. Please try again.',
+            ERROR_MSG: 'Sorry, I had trouble doing what you asked. Please try again.'
+        }
+    },
+}
